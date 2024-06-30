23529047e4e821c82b0d73af34083bccebbf8638
Ivan Shumilov <ivanshummi@gmail.com>
2024-06-30 14:15:40 +0200

    test: :white_check_mark: add separate tests

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import { Helloj3vl } from './helloj3vl';
 
 function init() {
   const domNode = document.getElementById('app');
-  const root = createRoot(domNode);
+  const root = ReactDOM.createRoot(domNode);
   root.render(<Helloj3vl />);
 }
 
