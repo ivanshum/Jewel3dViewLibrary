a212fa6fcbd0b08caa045f82fa1df8d260cec998
Ivan Shumilov <ivanshummi@gmail.com>
2024-07-02 18:09:51 +0200

    feat: :sparkles: Load options on init from object

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Viewer3d } from './3dviewer.js';
 
-function init() {
+function init({ ...config }) {
   const domNode = document.getElementById('app');
   const root = ReactDOM.createRoot(domNode);
-  root.render(<Viewer3d />);
+  root.render(<Viewer3d config={config} />);
 }
 
 export { init };
